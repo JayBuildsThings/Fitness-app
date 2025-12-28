0e443356bc9e077feee92722c3595c8cce8dc263
JayBuildsThings <104721193+JayBuildsThings@users.noreply.github.com>
2025-12-28 14:36:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'fittrack-v3';
+
+
+const CACHE_NAME = 'fittrack-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -35,5 +37,6 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
 
